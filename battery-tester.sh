18c85272fb4d6759051f1acc846bb0426c4da00f
Jakub Knetl <knetl.j@gmail.com>
2018-06-15 14:34:19 -0600

    [battery tester] Prompt for output file.

--- battery-tester.sh
+++ battery-tester.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 #
+# battery-tester.sh
+# =================
+# USAGE: battery-tester.sh [outfile]
+#
 # Script for estimating battery life. This script should run infinitely
 # until a battery of laptop dies. It writes time to an output file.
 
-OUTFILE=battery-status.txt
+OUTFILE={$1:battery-status.txt}
+
+if [ -e "$OUTFILE" ]; then
+    until [ $input == "y" -o $input == "n" ]; do
+        read -p "File $OUTFILE already exists do you want to overwrite it? [y/n]: "  input
+    done
+    if [ $input == "n" ]; then
+        echo "Exiting without overwriting file."
+    else
+        echo "Starting to monitor battery. Output goes into $OUTFILE"
+    fi
+fi
+
 
 while true ; do
     date +%T >> $OUTFILE
