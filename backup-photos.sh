7a2fb43c55e996ace5f2486c0d2b8615fbc959b5
Jakub Knetl <knetl.j@gmail.com>
2018-12-20 11:30:21 +0100

    [backup-photos] Remove default folders

--- backup-photos.sh
+++ backup-photos.sh
@@ -4,8 +4,13 @@
 # ================
 # A simple script for backing up photos from my laptop to ssd
 
-source_dir=${1:-$HOME/storage/fotky/}
-dest_dir=${2:-/run/media/$USER/SeagateBackupPlus/Multimedia/FOTO/}
+if [[ $# -lt 2 ]]; then
+  echo "usage: backup-photos.sh SOURCE_DIR DESTINATION_DIR"
+  exit 1
+fi
+
+source_dir=${1}
+dest_dir=${2}
 
 sync.sh "$source_dir" "$dest_dir"
 
