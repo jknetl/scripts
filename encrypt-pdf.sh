ca79bf7a8e3eb48c20b1cce5ce379397682067cc
Jakub Knetl <knetl.j@gmail.com>
2019-12-11 19:38:29 +0700

    [encrypt-pdf] Add usage

--- encrypt-pdf.sh
+++ encrypt-pdf.sh
@@ -3,6 +3,11 @@
 #
 # usage: encrypt-pdf.sh file-to-encrypt.pdf output-name.pdf
 
+if [ $# -ne 2 ]; then
+  echo "usage: encrypt-pdf.sh file-to-encrypt.pdf output-file.pdf"
+  exit 1
+fi
+
 echo "enter password: "
 read -rs pw
 
